4a3e834887f4d6414f69dc816e1ecab08a5f8590
Douglas Urner <dlu@canishe.com>
2020-01-16 00:47:43 -0800

    Building out AssignmentStatus

--- lib/assignment_status.rb
+++ lib/assignment_status.rb
@@ -54,23 +54,64 @@ module CAPI
     route = "/v1/courses/#{cid}/assignments/#{aid}/submissions/#{uid}"
     get(route)
   end
+
+  def self.assignment(cid, aid)
+    route = "/v1/courses/#{cid}/assignments/#{aid}"
+    get(route)
+  end
 end
 
 class AssignmentStatus
-  attr_accessor :assignment_id, :student_id
+  attr_accessor :assignment_id, :course_id, :student_id
   attr_reader :progress, :state
 
-  def initialize(student_id, assignment_id)
+  # State:
+  # - :pending (exists, but students are not expected to be working on it)
+  # - :open (past the the "available from" date)
+  # - :due (within X of due date - due today, due by next class)
+  # - :closed (submissions are no longer accepted)
+
+  # Progress Flags:
+  # - :not_started (no page views & no commits on assignment branch on GH) (0x0000)
+  # - :assignment_viewed () (0x0001)
+  # - :progressing () (0x0002)
+  # - :on_track () (0x0004)
+  # - :submitted_ontime (initial submission) (0x0008)
+  # - :submitted_late (initial submission) (0x0010)
+  # - :graded (clear on resubmission) (0x0020)
+  # - :pending_revisions (0x0040)
+  # - :resubmitted (0x0080)
+  # - :grade_posted (to gradebook of record) (0x0100)
+  # - :teacher_comments_pending () (0x1000)
+  # - :teacher_comments_read () (0x2000)
+  # - :student_comments_pending () (0x4000)
+  # - :student_comments_read () (0x8000)
+
+  def initialize(course_id, student_id, assignment_id)
+    @course_id = course_id
     @student_id = student_id
     @assignment_id = assignment_id
     @state = :pending
     @progress = :not_started
   end
+
+  def update_state
+    a = CAPI::assignment(@course_id, @assignment_id)
+  end
 end
 
 CAPI::base_url= 'https://canvas.instructure.com/api'
 
-response = CAPI::submission(1807744, 25199930, 13501849)
-p response.code
-response = CAPI::submission(1807744, 25199930, 1)
-p response.code
+# response = CAPI::submission(1807744, 25199930, 13501849)
+# p response.code
+# p s = JSON.parse(response.body)
+# # p response.headers
+# response = CAPI::submission(1807744, 25199930, 1)
+# p response.code
+#
+# response = CAPI::assignment(1807744, 13501849)
+# p response.code
+# p s = JSON.parse(response.body)
+# # p response.headers
+# response = CAPI::assignment(1807744, 1)
+# p response.code
--- test/assignment_status_test.rb
+++ test/assignment_status_test.rb
@@ -11,20 +11,34 @@ class AssignmentStatusTest < Minitest::Test
   let(:student) { 25199930 }
 
   def test_that_an_assignment_status_object_is_created
-    expected = AssignmentStatus.new(1, 42)
+    expected = AssignmentStatus.new(course, student, assignment)
     assert_instance_of AssignmentStatus, expected
   end
 
-  def test_that_assignment_status_has_assignment_id
-    as = AssignmentStatus.new(16, 28)
-    expected = as.assignment_id
-    assert_equal expected, 28
+  def test_that_assignment_status_has_course_id
+    as = AssignmentStatus.new(course, student, assignment)
+    assert_equal course, as.course_id
   end
 
   def test_that_assignment_status_has_student_id
-    as = AssignmentStatus.new(16, 28)
-    expected = as.student_id
-    assert_equal expected, 16
+    as = AssignmentStatus.new(course, student, assignment)
+    assert_equal student, as.student_id
+  end
+
+  def test_that_assignment_status_has_assignment_id
+    as = AssignmentStatus.new(course, student, assignment)
+    assert_equal assignment, as.assignment_id
+  end
+
+  def test_that_a_response_code_of_200_is_returned_for_a_good_assignment
+    response = CAPI::assignment(course, assignment)
+    assert_equal 200, response.code
+  end
+
+  def test_that_a_response_code_of_404_is_returned_for_an_invalid_assignment
+    #let(:assignment) { 1 }
+    response = CAPI::assignment(course, 1)
+    assert_equal 404, response.code
   end
 
   def test_that_a_response_code_of_200_is_returned_for_submitted_assignment
@@ -32,10 +46,15 @@ class AssignmentStatusTest < Minitest::Test
     assert_equal 200, response.code
   end
 
-  def test_that_a_response_code_of_404_is_returned_for_missing_assignment
+  def test_that_a_response_code_of_404_is_returned_for_missing_submission
     #let(:assignment) { 1 }
     response = CAPI::submission(course, student, 1)
     assert_equal 404, response.code
+  end
+
+  def test_that_a_canvas_assignment_is_returned
+    skip
+    response = CAPA::assignment(course, assignment)
   end
 
   def test_that_a_canvas_submission_is_returned
