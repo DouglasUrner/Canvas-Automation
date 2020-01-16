d9054ded55265eca82e2f46b6c7951fb6c243fb2
Douglas Urner <dlu@canishe.com>
2020-01-15 19:43:05 -0800

    Modularize Canvas API interface.

--- lib/assignment_status.rb
+++ lib/assignment_status.rb
@@ -1,3 +1,61 @@
+require 'json'
+require 'rest-client'
+
+# Local Gems
+# require 'secrets'
+
+module Secrets
+  class Error < StandardError; end
+  def self.source(path)
+    # Inspired by user takeccho at http://stackoverflow.com/a/26381374/3849157
+    # Sources sh-script or env file and imports resulting environment
+    fail(ArgumentError, "File #{path} missing or unreadable.") \
+       unless File.exist?(path)
+
+    _env_hash_str = `env -i sh -c 'set -a; source #{path} && ruby -e "p ENV"'`
+    fail(ArgumentError, 'Malformed environment in #{path}.') \
+       unless _env_hash_str.match(/^\{("[^"]+"=>".*?",\s*)*("[^"]+"=>".*?")\}$/)
+
+    _env_hash = eval(_env_hash_str)
+     %w[ SHLVL PWD _ ].each{ |k| _env_hash.delete(k) }
+    _env_hash.each{ |k,v| ENV[k] = v }
+  end
+end
+
+module CAPI
+  # Canvas API interface.
+  class Error < StandardError; end
+
+  if (ENV['ACCESS_TOKEN'] == nil)
+    Secrets::source('private/ENV')
+  end
+
+  def self.base_url=(base); @base_url = base; end
+  def self.base_url; @base_url; end
+
+  def self.headers
+    {
+      Authorization: "Bearer #{ENV['ACCESS_TOKEN']}"
+    }
+  end
+
+  def self.get(route)
+    begin
+      response = RestClient.get(base_url + route, headers)
+      response
+    rescue => e
+      # //p e.response.code
+      # //e.response
+      e.response
+    end
+  end
+
+  def self.submission(cid, uid, aid)
+    route = "/v1/courses/#{cid}/assignments/#{aid}/submissions/#{uid}"
+    get(route)
+  end
+end
+
 class AssignmentStatus
   attr_accessor :assignment_id, :student_id
   attr_reader :progress, :state
@@ -9,3 +67,10 @@ class AssignmentStatus
     @progress = :not_started
   end
 end
+
+CAPI::base_url= 'https://canvas.instructure.com/api'
+
+response = CAPI::submission(1807744, 25199930, 13501849)
+p response.code
+response = CAPI::submission(1807744, 25199930, 1)
+p response.code
--- test/assignment_status_test.rb
+++ test/assignment_status_test.rb
@@ -4,6 +4,12 @@ require 'minitest/pride'
 require_relative '../lib/assignment_status'
 
 class AssignmentStatusTest < Minitest::Test
+  extend MiniTest::Spec::DSL
+
+  let(:course) { 1807744 }
+  let(:assignment) { 13501849 }
+  let(:student) { 25199930 }
+
   def test_that_an_assignment_status_object_is_created
     expected = AssignmentStatus.new(1, 42)
     assert_instance_of AssignmentStatus, expected
@@ -21,11 +27,19 @@ class AssignmentStatusTest < Minitest::Test
     assert_equal expected, 16
   end
 
-  def test_that_a_canvas_submission_is_returned
-    skip
+  def test_that_a_response_code_of_200_is_returned_for_submitted_assignment
+    response = CAPI::submission(course, student, assignment)
+    assert_equal 200, response.code
   end
 
-  def test_that_a_404_error_is_returned
+  def test_that_a_response_code_of_404_is_returned_for_missing_assignment
+    #let(:assignment) { 1 }
+    response = CAPI::submission(course, student, 1)
+    assert_equal 404, response.code
+  end
+
+  def test_that_a_canvas_submission_is_returned
     skip
+    response = CAPA::submission(course, student, assignment)
   end
 end
