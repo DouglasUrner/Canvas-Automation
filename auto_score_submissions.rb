89f8be5d655f334018e47cf65ed5faee5aad6cd9
Douglas Urner <dlu@canishe.com>
2020-02-27 09:05:37 -0800

    Auto scoring for submitted URLs

--- auto_score_submissions.rb
+++ auto_score_submissions.rb
@@ -6,10 +6,12 @@ require_relative 'lib/modules/capi'
 
 CAPI::base_url= 'https://canvas.instructure.com/api'
 
-def get_assignment_repo(desc)
+# Parse the HTML from the assignment description to generate the URL
+# of the auto_score module in the source repo on GitHub.
+def get_scorer_url(desc)
   host = 'https://raw.githubusercontent.com'
   branch = 'master'
-  path = 'assessment/auto-score.rb'
+  path = 'assessment/auto_score.rb'
 
   pages, repo = desc.gsub(/^.*https:\/\//, '').gsub(/\".*$/, '').split('/')
   org = (pages.split('.'))[0]
@@ -17,8 +19,54 @@ def get_assignment_repo(desc)
   return "#{host}/#{org}/#{repo}/#{branch}/#{path}"
 end
 
-response = JSON.parse(CAPI::assignment(1822662, 13795872))
+def download_scorer(url)
+  scorer_name = "score-#{@opts[:assignment]}.rb"
+  cmd = "curl #{url} > #{scorer_name}"
+  # TODO: check return - execute command method.
+  # XXX: There is a potential injection attack here since the scorer URL is
+  #      inferred from the assignment repository.
+  %x( #{cmd} )
+  return scorer_name
+end
 
-repo = get_assignment_repo(response['description'])
+if (__FILE__ == $0)
+  require 'optparse'
 
-p repo
+  @opts = {
+    debug: false,
+    verbose: false,
+  }
+
+  OptionParser.new do |o|
+    o.banner = "Usage: #{$0} [options]"
+
+    o.on('-a ASSIGNMENT') { |v| @opts[:assignment] = v }
+    o.on('-c COURSE')     { |v| @opts[:course] = v }
+    o.on('-d')            { |v| @opts[:debug] = true }
+    o.on('-s STUDENT')    { |v| @opts[:student] = v }
+    o.on('-T TMPDIR')     { |v| @opts[:tmp_dir] = v }
+    o.on('-v')            { |v| @opts[:verbose] = true }
+  end.parse!
+
+  # XXX: need to handle failed requests / bad args.
+  response = CAPI::assignment(@opts[:course], @opts[:assignment])
+
+  scorer_url = get_scorer_url(response['description'])
+  scorer = download_scorer(scorer_url)
+
+  if (@opts[:student])
+    response = CAPI::submission(@opts[:course], @opts[:assignment], @opts[:student])
+    puts response
+  else
+    response = CAPI::submissions(@opts[:course], @opts[:assignment], %w[user])
+    response.each do |s|
+      if (s['url'])
+        puts "#{s['user']['name']} (#{s['id']}): #{s['workflow_state']} #{s['url']}"
+        score_cmd = "ruby #{scorer} #{s['url']}"
+        puts score_cmd  if (debug)
+        score = %x( #{score_cmd} )
+        puts score
+      end
+    end
+  end
+end
