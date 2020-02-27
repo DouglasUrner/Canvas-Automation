8fed716a3886fd8a417ecc8fa49b73dfe2eee357
Douglas Urner <dlu@canishe.com>
2020-02-26 22:25:16 -0800

    Allow includes in URL

--- lib/modules/capi.rb
+++ lib/modules/capi.rb
@@ -22,22 +22,35 @@ module CAPI
   end
 
   def self.get(route)
+    route += "?include[]=#{append_includes(includes)}" if (includes != '')
     begin
       response = RestClient.get(base_url + route, headers)
     rescue => e
-      # //p e.response.code
-      # //e.response
       e.response
     end
   end
 
-  def self.submission(cid, uid, aid)
-    route = "/v1/courses/#{cid}/assignments/#{aid}/submissions/#{uid}"
-    get(route)
+  def self.assignment(cid, aid, includes = '')
+    route = "/v1/courses/#{cid}/assignments/#{aid}"
+    get(route, includes)
   end
 
-  def self.assignment(cid, aid)
-    route = "/v1/courses/#{cid}/assignments/#{aid}"
-    get(route)
+  def self.submission(cid, uid, aid, includes = '')
+    route = "/v1/courses/#{cid}/assignments/#{aid}/submissions/#{uid}"
+    get(route, includes)
+  end
+
+  def self.submissions(cid, uid, includes = '')
+    route = "/v1/courses/#{cid}/assignments/#{aid}/submissions"
+    get(route, includes)
+  end
+
+  def self.append_includes(list)
+    # XXX: Guard against empty list?
+    includes = ''
+    list.each do |i|
+      includes += (includes == '' ? i : ',' + i)
+    end
+    return "?include[]=#{includes}"
   end
 end
