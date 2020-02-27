4473fe7e5a8c4cbc7d4a220c96af27e5794b15d2
Douglas Urner <dlu@canishe.com>
2020-02-26 22:24:31 -0800

    Break out Canvas API tests

---
+++ test/capi_test.rb
@@ -0,0 +1,22 @@
+gem 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/modules/capi'
+
+class AssignmentStatusTest < Minitest::Test
+  extend MiniTest::Spec::DSL
+
+  let(:course) { 1807744 }
+  let(:assignment) { 13501849 }
+  let(:student) { 25199930 }
+
+  def test_append_includes_with_one_item
+    includes = CAPI::append_includes(%w[user])
+    assert_equal '?include[]=user', includes
+  end
+
+  def test_append_includes_with_multiple_items
+    includes = CAPI::append_includes(%w[one two three])
+    assert_equal '?include[]=one,two,three', includes
+  end
+end
