be290148df3103dbab8046a434a24d0691815f5f
Douglas Urner <dlu@canishe.com>
2020-03-02 20:37:27 -0800

    Modularizing

--- auto_score_assignment.rb
+++ auto_score_assignment.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby
 
+require_relative 'lib/modules/casl'
+require_relative 'lib/comments'
+
 # Clone a GitHub repository holding a Unity project and do mechanical checks
 # for completion of the assignment and for correctness. This file is the
 # boilerplate for the test and is deployed in the assignment repository. The
@@ -12,6 +15,7 @@ OPTS = {
   base_url: 'https://github.com/',
   branch: '',
   clone: 'git clone',
+  comments_file: 'lib/comments.yml',
   debug: false,
   empty_script_size: 323,
   gitignore_size: 500,
@@ -23,10 +27,7 @@ OPTS = {
 }
 
 @score = 0
-@comments = []
 @resubmit = false
-
-require_relative 'lib/modules/casl'
 
 if (__FILE__ == $0)
   require 'optparse'
@@ -34,16 +35,23 @@ if (__FILE__ == $0)
   OptionParser.new do |o|
     o.banner = "Usage: #{$0} [options]"
 
-    o.on('-b BRANCH') { |v| OPTS[:branch] = v }
-    o.on('-d')        { |v| OPTS[:debug] = true }
-    o.on('-T TMPDIR') { |v| OPTS[:tmp_dir] = v }
-    o.on('-v')        { |v| OPTS[:verbose] = true }
+    o.on('-b BRANCH')   { |v| OPTS[:branch] = v }
+    o.on('-d')          { |v| OPTS[:debug] = true }
+    o.on('-M COMMENTS') { |v| OPTS[:comments_file] = v }
+    o.on('-T TMPDIR')   { |v| OPTS[:tmp_dir] = v }
+    o.on('-v')          { |v| OPTS[:verbose] = true }
   end.parse!
 
   remote = ARGV.pop
 
+  @comments = Comments.new(OPTS[:comments_file])
+  # comments.push('unable_to_clone', 'oops!')
+  # puts comments.dump()
+
+  # Clone the project repository.
   points, comment = CASL::clone_and_score(remote)
+  CASL::done(@score, @comments, true) if (points == 0) # Clone failed, quit
+  @score += points
 
   CASL::done(@score, @comments, @resubmit)
-
 end
