4b5a1c880cf7b86fc8d9ea8342b287e28053dc50
Douglas Urner <dlu@canishe.com>
2020-03-02 20:37:06 -0800

    Modularizing

--- lib/modules/casl.rb
+++ lib/modules/casl.rb
@@ -1,6 +1,7 @@
 # Canvas Automation Support Library
 
 module CASL
+  require 'json'
   require 'open3'         # Used by clone().
   class << self
     # Per GBS - squelch undefined method `popen3' error that cropped up
@@ -8,9 +9,12 @@ module CASL
     include Open3
   end
   require 'pathname'
+  require 'pry'
+  require 'yaml'
 
-  def self.done(score, comments, resubmit)
-    if (OPTS[:debug])
+  def self.done(score, comments, revise)
+    debug = OPTS[:debug]
+    if (debug)
       puts "done(): resubmit = #{resubmit}; score = #{score};\n"               +
            "comments ="
       comments.each do |c|
@@ -18,18 +22,19 @@ module CASL
       end
     end
 
-    msg = 'After correcting any problems you may resubmit up until the assignment closes.'
-    comments.push(msg) if (resubmit || score <= (OPTS[:max_points] * OPTS[:resubmit_threshold]))
-    puts score
-    if ( comments.length > 1 || comments[0].length > 0 )
-      comments.each do |c|
-        puts "#{c}\n\n" if (c != nil && c.length > 0)
-      end
-    end
+    comments.resubmit(score, revise)
+
+    fb = {}
+    fb['score'] = score
+    fb['comments'] = comments.dump()
+    msg = ''
+
+    puts fb.to_json
+    # Because we may be called part way through...
     exit
   end
 
-  def self.clone_and_score( url, path = local_from_remote(url) )
+  def self.clone_and_score(url, path = local_from_remote(url))
 
     # Attempt to clone the repository.
     #
@@ -38,8 +43,6 @@ module CASL
     # - Failure: the URL "looks" good, but failed to clone - perhaps a private repo.
     # - Failure: the URL does not match the expected pattern.
 
-    # path = local_from_remote(url) if (path == '')
-
     if (clone(url, path) == 0)
       # Success
       points = 1
@@ -47,6 +50,7 @@ module CASL
     else
       # XXX: clean up regexp handling - don't bury it here.
       points = 0
+      @comments.dump()
       msg = "I was unable to clone your repository: \'#{url}\'. "
       if (url.match(/\/[Pp]rototype-*[1-5]$/))
         # URL seems plausible
@@ -63,7 +67,13 @@ module CASL
     return points, msg
   end
 
-  def check_repo_sanity(path, min, max)
+  def self.local_from_remote(url)
+    local = url.gsub(OPTS[:base_url], '')
+    local = "#{OPTS[:tmp_dir]}/#{local}" if OPTS[:tmp_dir]
+    return local
+  end
+
+  def self.check_repo_sanity(path, min, max)
     # Check that the repository looks sane. We expect:
     # - An Assets folder
     # - A .gitignore File
@@ -118,7 +128,7 @@ module CASL
     return points, msg
   end
 
-  def checkout_branch_and_score(path, branch)
+  def self.checkout_branch_and_score(path, branch)
     if (checkout_branch(path, branch) == 0)
       # Success
       points = 1
@@ -131,7 +141,7 @@ module CASL
             "back) it is also worth developing a habit of consistancy (and "     +
             "tracking directions)."
     end
-    if (OPTS[:debug])
+    if (debug)
       puts "checkout_branch_and_score(): #{points.to_s}: #{msg}"
     end
     return points, msg
@@ -149,7 +159,7 @@ module CASL
     return wait.value
   end
 
-  def checkout_branch(path, branch)
+  def self.checkout_branch(path, branch)
     # TODO: do a better job with messages from Git.
 
     cmd = "cd #{path}; git checkout #{branch}"
@@ -211,7 +221,7 @@ module CASL
     return nil
   end
 
-  def check_script(path, name)
+  def self.check_script(path, name, threshold = 3, limit = 'Assets')
     # TODO: take a block to check specifics
     #       check class name for match with file name
     #       check class name for default value
@@ -221,9 +231,20 @@ module CASL
     #       check for "reasonable" misspellings
     debug = OPTS[:debug]
     script_path = "#{path}/#{name}"
-    script = Pathname.new(script_path)
-    if (script.file?)
-      if (script.size > OPTS[:empty_script_size])
+
+    hits = fuzzy_file_match(script_path, limit, threshold)
+    case (hits[0][:distance])
+    when -1
+      # Nothing promising.
+      puts "check_script(#{script_path}): not found" if (debug)
+      points = 0
+      msg = "I couldn't find your \'#{name}\' script.\n\n"                       +
+            "Make sure you've named it correctly (and be sure to change the "    +
+            "class name if rename the file), or perhaps you created it "         +
+            "in another folder."
+    when 0
+      # Exact match.
+      if (Pathname.new(script_path).size > @opts[:empty_script_size])
         puts "check_script(#{script_path}): found, size = #{script.size}" if (debug)
         points = 1
         msg = ''
@@ -234,20 +255,92 @@ module CASL
               "changes before committing?"
       end
     else
-      puts "check_script(#{script_path}): not found" if (debug)
+      # A possiblity - but misnamed or in the wrong folder.
       points = 0
-      msg = "I couldn't find your \'#{name}\' script in the 'Scripts' folder. "  +
-            "Make sure you've named it correctly (and be sure to change the "    +
-            "class name if rename the file), or perhaps you created it "         +
-            "in another folder."
+      possibilities = []
+      hits.each do |h|
+        possibilities.push(h) if (h[:distance] <= threshold)
+      end
+      msg = "I didn't find a script called #{name}, but I did find "             +
+            "#{possibilities.length} possible match#{"es" if possibilities.length > 1}:\n"
+      possibilities.each do |p|
+        msg += " #{p[:name]}\n"
+      end
+      msg += "If #{(possibilities.length == 1) ? 'this' : 'one of these'} is "   +
+             "right, it should be renamed to #{name}. You will probabaly need "   +
+             "to update the class name in the file to match."
     end
+
     puts "check_script(#{script_path}): returning - points = #{points}; msg = \'#{msg}\'"  if (debug)
     return points, msg
   end
 
-  def local_from_remote(remote)
-    local = remote.gsub(OPTS[:base_url], '')
-    local = "#{OPTS[:tmp_dir]}/#{local}" if (OPTS[:tmp_dir])
-    local.gsub(/^.*\//, '')
+  # Levenshtein algorithm for calculating the "distance" between two words.
+  # Use to identify likely misspellings of file names.
+  #
+  # Taken from: https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance#Ruby
+  #
+  def self.levenshtein(first, second)
+    m, n = first.length, second.length
+    return m if n == 0
+    return n if m == 0
+
+    # Create our distance matrix
+    d = Array.new(m+1) {Array.new(n+1)}
+    0.upto(m) { |i| d[i][0] = i }
+    0.upto(n) { |j| d[0][j] = j }
+
+    1.upto(n) do |j|
+      1.upto(m) do |i|
+        d[i][j] = first[i-1] == second[j-1] ? d[i-1][j-1] : [d[i-1][j]+1,d[i][j-1]+1,d[i-1][j-1]+1,].min
+      end
+    end
+    d[m][n]
   end
+
+  # Check for the existance of a file - with allowances for misspellings and
+  # being in the wrong directory. Returns an array of hashes, ordered by the
+  # "fuzziness" of the file name and then by the "fuzziness" of the directory
+  # if a file with a Levenshtein distance of <= "threshold" is found. Else, we
+  # return an arry with a single entry with a distance of -1.
+  #
+  # TODO: Use relative_path_from() to report a better path, perhaps by popping
+  #       up the path with parent() until we reach the directory above the limit
+  #       and reporting the relative path from there.
+  def self.fuzzy_file_match(name, limit = 'Assets', threshold = 3)
+    @distances = []
+    path = Pathname.new(name)
+
+    # See if the file exists as specified
+    if path.exist?
+      @distances.push({'directory': path.parent, 'name': path.basename, 'distance': 0})
+      return @distances
+    end
+
+    # Check if the folder that should hold 'name' exists
+    # and if there is a close match to the name.
+    scan_dirs(path.parent, path.basename, limit)
+    @distances.sort_by! { |k| k[:distance] }
+    # puts @distances
+    return @distances if (@distances[0][:distance] <= threshold)
+
+    # Nothing seems to match
+    @distances = []
+    @distances.push({'directory': nil, 'name': nil, 'distance': -1})
+    return @distances
+  end
+
+  def self.scan_dirs(dir, name, limit)
+    dir.each_entry do |e|
+      next if (e.fnmatch?('.') || e.fnmatch?('..') || e.fnmatch('*.meta'))
+      # Would like to do this, but it fails with the message:
+      # No such file or directory @ rb_file_s_ftype - PlayerController.cs (Errno::ENOENT)
+      # next if ((e.ftype == 'directory') || e.fnmatch('*.meta'))
+      d = levenshtein(name.to_s, e.to_s)
+      f = {'directory': dir,'name': e, 'distance': d}
+      @distances.push(f)
+    end
+    scan_dirs(dir.parent, name, limit) if (dir.parent.to_s.match?(limit))
+  end
+
 end
